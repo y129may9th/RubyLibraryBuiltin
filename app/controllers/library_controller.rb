184cd205f93bc8c5827b5b025a87d9e987fb9742
y129may9th <y129may9th@gmail.com>
2019-08-27 03:24:03 +0900

    Make scraping

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -1,4 +1,22 @@
+require 'nokogiri'
+require 'open-uri'
+
 class LibraryController < ApplicationController
   def top
+    url = 'https://docs.ruby-lang.org/ja/2.6.0/library/_builtin.html'
+
+    charset = nil
+
+    html = open(url) do |f|
+        charset = f.charset
+        f.read
+    end
+
+    @library_url = []
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+    doc.xpath('//td[@class="signature"]').each do |node|
+    @library_url << node.css('a').attribute('href').value
+    # p @url
+    end
   end
 end
--- lib/tasks/scrape.rake
+++
@@ -1,2 +0,0 @@
-namespace :scrape do
-end
